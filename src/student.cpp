8e48909ae2c3aecd612039fc7e9e75afb523dd4e
Zaxius01 <nacount243@gmail.com>
2026-09-23 17:39:24 +0700

    iningepush

--- src/student.cpp
+++ src/student.cpp
@@ -89,22 +89,78 @@ void tutupSemuaTab(CNode*& head) {
 
 // SOAL 1
 bool bukaHalaman(DNode*& head, DNode*& tail, int nomor) {
-    return false;
+    DNode *baru = new DNode;
+    baru->data = nomor;
+    baru->next = nullptr;
+    baru->prev = tail;
+
+    if (tail != nullptr) {
+        tail->next = baru;
+    } else {
+        head = baru;
+    }
+
+    tail = baru;
+    return true;
 }
 
 // SOAL 2
 int riwayatMundur(DNode* tail, int* keluaran) {
-    return 0;
+    int n = 0;
+    DNode *p = tail;
+    while (p != nullptr) {
+        keluaran[n] = p->data;
+        n++;
+        p = p->prev;
+    }
+    return n;
 }
 
 // SOAL 3
 bool hapusHalaman(DNode*& head, DNode*& tail, int nomor) {
-    return false;
+     DNode *p = head;
+    while (p != nullptr && p->data != nomor) {
+        p = p->next;
+    }
+    if (p == nullptr) {
+        return false;
+    }
+
+    if (p->prev != nullptr) {
+        p->prev->next = p->next;
+    } else {
+        head = p->next;
+    }
+
+    if (p->next != nullptr) {
+        p->next->prev = p->prev;
+    } else {
+        tail = p->prev;
+    }
+
+    delete p;
+    return true;
 }
 
 // SOAL 4
 bool bukaTab(CNode*& head, int nomor) {
-    return false;
+    CNode *baru = new CNode;
+    baru->data = nomor;
+
+    if (head == nullptr) {
+        baru->next = baru;
+        head = baru;
+        return true;
+    }
+
+    CNode *temp = head;
+    while (temp->next != head) {
+        temp = temp->next;
+    }
+
+    temp->next = baru;
+    baru->next = head;
+    return true;
 }
 
 // =============================================================================
